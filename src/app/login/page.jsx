cdf8584bee86143e31fd4f5f9ac205f0e8254313
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-08-25 12:27:58 -0300

    feat: create login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,3 +1,22 @@
-export default function Login() {
-  return <h1>Página de login</h1>;
+import LoginForm from "@/components/custom/loginForm";
+import { auth } from "../api/auth/auth";
+import { validateExp } from "@/utils/validateExp";
+import { redirect } from "next/navigation";
+import dynamic from "next/dynamic";
+
+const ExpiresModal = dynamic(() =>
+  import("@/components/custom/ExpiresModal", { ssr: false })
+);
+
+export default async function Login() {
+  const session = await auth();
+  if (session && validateExp(session.expires)) {
+    redirect("/dashboard");
+  }
+  return (
+    <div className="flex items-center justify-center w-full h-full  flex-col ">
+      <ExpiresModal />
+      <LoginForm />
+    </div>
+  );
 }
