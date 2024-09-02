9a31b79f0c46efc499850e9c50f9883b6281adb6
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-09-01 22:14:29 -0300

    feat: create custom components

---
+++ src/components/custom/formFieldTextArea.jsx
@@ -0,0 +1,30 @@
+import { cn } from "@/lib/utils";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "../ui/form";
+
+export default function FormFieldTextArea({ name, label, control, className }) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className={cn("flex flex-col", className)}>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <textarea
+              className="bg-background  rounded-md border border-input  ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              {...field}
+              style={{ resize: "none" }}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
