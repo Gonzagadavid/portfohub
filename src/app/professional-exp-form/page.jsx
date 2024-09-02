b4bd830c25954d2a0ce24020b3c3724c36a7641f
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-09-01 22:15:41 -0300

    feat: create professional experience form page

--- src/app/professional-exp-form/page.jsx
+++ src/app/professional-exp-form/page.jsx
@@ -1,3 +1,49 @@
+"use client";
+
+import { useState } from "react";
+import ProfessionalFormItem from "./_components/professionalFormItem";
+import { Button } from "@/components/ui/button";
+import DynamicList from "@/components/custom/dynamicList";
+
 export default function ProfessionalExpForm() {
-  return <h1>Formulário experiência profissional</h1>;
+  const [professionalList, setProfessionalList] = useState([]);
+  const [newItem, setNewItem] = useState(false);
+
+  const addItemInList = (item) => {
+    setProfessionalList((prev) => [...prev, item]);
+    setNewItem(false);
+  };
+
+  const removeItemList = (index) => {
+    setProfessionalList((prev) => [
+      ...prev.slice(0, index),
+      ...prev.slice(index + 1)
+    ]);
+  };
+
+  const updateItemList = (item, index) => {
+    setProfessionalList((prev) => [
+      ...prev.slice(0, index),
+      item,
+      ...prev.slice(index + 1)
+    ]);
+  };
+
+  return (
+    <div className="flex flex-col items-center w-[80%] mx-auto p-5">
+      <h1>Formulário experiência profissional</h1>
+      <DynamicList
+        addItemInList={addItemInList}
+        updateItemList={updateItemList}
+        removeItemList={removeItemList}
+        propertyKey="company"
+        addLabel="Adicionar nova experiência profissional"
+        list={professionalList}
+        newItem={newItem}
+        setNewItem={setNewItem}
+      >
+        <ProfessionalFormItem />
+      </DynamicList>
+    </div>
+  );
 }
