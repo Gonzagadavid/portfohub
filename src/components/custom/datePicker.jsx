8bbe0116b36c868c8c373cd877c54dd379cbbfc2
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-09-01 22:14:29 -0300

    feat: create custom components

---
+++ src/components/custom/datePicker.jsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from "@/components/ui/popover";
+
+export default function DatePicker({ name, label, control, className }) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className={cn("flex flex-col", className)}>
+          <FormLabel>{label}</FormLabel>
+          <Popover>
+            <PopoverTrigger asChild>
+              <FormControl>
+                <Button
+                  variant={"outline"}
+                  className={cn(
+                    "w-[240px] pl-3 text-left font-normal",
+                    !field.value && "text-muted-foreground"
+                  )}
+                >
+                  {field.value ? (
+                    format(field.value, "dd/MM/yyyy")
+                  ) : (
+                    <span>Selecione a data</span>
+                  )}
+                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                mode="single"
+                selected={field.value}
+                onSelect={field.onChange}
+              />
+            </PopoverContent>
+          </Popover>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
---
+++ src/components/custom/dynamicList.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { cloneElement } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function DynamicList({
+  list,
+  propertyKey,
+  children,
+  updateItemList,
+  addItemInList,
+  removeItemList,
+  addLabel,
+  newItem,
+  setNewItem
+}) {
+  return (
+    <>
+      {list.map((item, index) =>
+        cloneElement(children, {
+          key: `${item[propertyKey]}-${index}`,
+          item,
+          addItemInList: updateItemList,
+          index,
+          removeItemList
+        })
+      )}
+      {newItem && cloneElement(children, { addItemInList })}
+      <div className=" flex justify-end p-5 w-full">
+        {!newItem && (
+          <Button onClick={() => setNewItem(true)}>{addLabel}</Button>
+        )}
+      </div>
+    </>
+  );
+}
