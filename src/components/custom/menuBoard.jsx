a94653a4429f2083ba9d60357bd7a03484acdb88
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-09-08 13:57:15 -0300

    feat: create private routes

--- src/components/custom/menuBoard.jsx
+++ src/components/custom/menuBoard.jsx
@@ -2,10 +2,16 @@ import { Routes, RoutesInfo } from "@/constants/routes";
 import Link from "next/link";
 import { Card, CardContent, CardDescription } from "../ui/card";
 
+const routesNotListed = [Routes.DASHBOARD];
+
+const routesMenu = Object.keys(RoutesInfo).filter(
+  (route) => !routesNotListed.includes(route)
+);
+
 export default function MenuBoard() {
   return (
     <div className="flex justify-around  w-[60%] p-10 mt-10">
-      {Object.keys(RoutesInfo).map((route) => (
+      {routesMenu.map((route) => (
         <Link key={route} href={route}>
           <Card className=" flex flex-col items-center w-40 h-44 p-2">
             <CardContent>
