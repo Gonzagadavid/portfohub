496bc2c098cf50b53247941abfd9d5ea8795fde0
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-09-08 18:51:23 -0300

    feat: send professional form to back

--- src/lib/sendRequest.js
+++ src/lib/sendRequest.js
@@ -1,12 +1,19 @@
 "use server";
+
+import { auth } from "@/app/api/auth/auth";
+
 const baseUrlApi = process.env.BASE_URI_API;
 
 export async function sendRequest(route, { arg }) {
+  const session = await auth();
+  const authorization = session?.token?.accessToken;
+
   const res = await fetch(`${baseUrlApi}${route}`, {
     method: "POST",
     body: JSON.stringify(arg),
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      authorization
     }
   });
   const response = await res.json().catch(() => null);
