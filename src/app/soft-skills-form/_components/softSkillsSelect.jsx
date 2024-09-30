07021cb05db81702327dabb02dafa5829fa655af
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-09-29 22:25:34 -0300

    feat: create soft skills form

---
+++ src/app/soft-skills-form/_components/softSkillsSelect.jsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useState } from "react";
+import { Command, CommandItem, CommandList } from "@/components/ui/command";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { soft_skills } from "@/constants/softSkills";
+
+export default function SoftSkillsSelect({ addSkill, skillList }) {
+  const [input, setInput] = useState("");
+  const [showList, setShowList] = useState(false);
+
+  const options = soft_skills.filter(
+    (skill) =>
+      !skillList.has(skill) && skill.toLowerCase().includes(input.toLowerCase())
+  );
+
+  const onChange = (event) => {
+    if (!showList) setShowList(true);
+    setInput(event.target.value);
+  };
+  const onClick = (event) => {
+    if (showList) setShowList(false);
+    setInput(event.target.innerText);
+  };
+  const onFocus = () => {
+    setShowList(true);
+  };
+
+  const onAddStack = () => {
+    if (!input) return;
+    addSkill(input);
+    setInput("");
+  };
+
+  return (
+    <Command className="rounded-lg border-none shadow-md md:min-w-[450px] h-[200px]">
+      <div className="flex mt-10 justify-between">
+        <Input
+          placeholder="Selecione suas stacks..."
+          onChange={onChange}
+          value={input}
+          onFocus={onFocus}
+        />
+        <Button className="ml-5" onClick={onAddStack}>
+          Adicionar
+        </Button>
+      </div>
+      <CommandList
+        className={`${showList ? "visible" : "invisible"} w-[335px]`}
+      >
+        {options.map((skill, i) => (
+          <CommandItem key={`${skill}${i}`}>
+            <div className="w-full flex justify-between px-5" onClick={onClick}>
+              {skill}
+            </div>
+          </CommandItem>
+        ))}
+      </CommandList>
+    </Command>
+  );
+}
