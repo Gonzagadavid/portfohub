b7e192823ec211a9e62e071b55ccf63648fd7b69
damacosta <d.mariano.costa@gmail.com>
2024-10-08 23:17:02 -0300

    wip

--- src/components/custom/formFieldTextArea.jsx
+++ src/components/custom/formFieldTextArea.jsx
@@ -7,7 +7,12 @@ import {
   FormMessage
 } from "../ui/form";
 
-export default function FormFieldTextArea({ name, label, control, className }) {
+export default function PersonalDataFormItem({ 
+  name, 
+  label, 
+  control, 
+  className 
+}) {
   return (
     <FormField
       control={control}
@@ -17,9 +22,9 @@ export default function FormFieldTextArea({ name, label, control, className }) {
           <FormLabel>{label}</FormLabel>
           <FormControl>
             <textarea
-              className="bg-background  rounded-md border border-input  ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              className="bg-background rounded-md border border-input ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               {...field}
-              style={{ resize: "none" }}
+              style={{ resize: "none", height: "150px" }} // Define a altura desejada aqui
             />
           </FormControl>
           <FormMessage />
