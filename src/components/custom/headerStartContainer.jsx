ad543c7ffc2f38ddb110f8b7d3dcb098a7a8247b
alisson ai <alissonaquino2013@gmail.com>
2024-10-17 21:17:21 -0300

    Menu Drawer

--- src/components/custom/headerStartContainer.jsx
+++ src/components/custom/headerStartContainer.jsx
@@ -1,9 +1,72 @@
-import { Menu } from "lucide-react";
+import { Menu, LogOut, Home } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Link from "next/link"; 
+import { Routes, RoutesInfo } from "@/constants/routes";
+import { logout } from "@/lib/actions";
+
+const routesMenu = Object.keys(RoutesInfo);
 
 export function HeaderStartContainer() {
   return (
-    <div>
-      <Menu size={30} />
+    
+    <div className="my-4">
+     
+      <Sheet >
+        <SheetTrigger asChild>
+          <button>
+            <Menu size={30} />
+          </button>
+        </SheetTrigger>
+
+        <SheetContent side="left" >
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+          </SheetHeader>
+          
+          <hr className="my-4" />
+
+          <div className="flex flex-col gap-4 px-4">
+            <Link href={Routes.DASHBOARD}>
+              <button className="flex items-center gap-2 text-left text-sm font-medium">
+                <Home size={18} /> Página Inicial
+              </button>
+            </Link>
+
+            {routesMenu
+              .filter((route) => RoutesInfo[route]?.label) 
+              .map((route) => (
+                <Link href={route} key={route}>
+                  <button className="flex items-center gap-2 text-left text-sm font-medium">
+                    {RoutesInfo[route].icon && RoutesInfo[route].icon(18)} 
+                    {RoutesInfo[route].label} 
+                  </button>
+                </Link>
+              ))}
+
+            <form action={logout}>
+              <button
+                type="submit"
+                className="flex items-center gap-2 text-left text-sm font-medium text-red-500"
+              >
+                <LogOut size={18} /> Sair
+              </button>
+            </form>
+          </div>
+
+          <SheetFooter>
+            <SheetClose asChild>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 }
