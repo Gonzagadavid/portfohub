5fe160f682ae78caacbe996e5303b6795e32794e
damacosta <d.mariano.costa@gmail.com>
2024-11-06 22:29:57 -0300

    feat: add responsive projects form

--- src/app/projects-form/_components/StackSelect.jsx
+++ src/app/projects-form/_components/StackSelect.jsx
@@ -20,13 +20,13 @@ export default function StackSelect({ addStack, stackList }) {
     if (!showList) setShowList(true);
     setInput(event.target.value);
   };
+
   const onClick = (event) => {
-    if (showList) setShowList(false);
     setInput(event.target.innerText);
+    setShowList(false);
   };
-  const onFocus = () => {
-    setShowList(true);
-  };
+
+  const onFocus = () => setShowList(true);
 
   const onAddStack = () => {
     if (!input) return;
@@ -35,24 +35,32 @@ export default function StackSelect({ addStack, stackList }) {
   };
 
   return (
-    <Command className="rounded-lg border-none shadow-md md:min-w-[450px] h-[150px]">
-      <div className="flex mt-10 justify-between">
+    <Command className="rounded-lg border-none shadow-md md:min-w-[450px]">
+      <div className="flex flex-col md:flex-row md:items-center md:space-x-4">
         <Input
           placeholder="Selecione as stacks usadas no projeto..."
           onChange={onChange}
           value={input}
           onFocus={onFocus}
+          className="w-full md:w-[450px]"
         />
-        <Button type="button" className="ml-5" onClick={onAddStack}>
+        <Button
+          type="button"
+          className="mt-4 md:mt-0 md:w-auto"
+          onClick={onAddStack}
+        >
           Adicionar Stack
         </Button>
       </div>
       <CommandList
-        className={`${showList ? "visible" : "invisible"} w-[335px]`}
+        className={`${showList ? "visible" : "hidden"} w-full md:w-[450px] mt-4 md:mt-2`}
       >
         {options.slice(0, 10).map((title, i) => (
-          <CommandItem key={`${title}${i}`}>
-            <div className="w-full flex justify-between px-5" onClick={onClick}>
+          <CommandItem key={`${title}${i}`} className="px-3 py-2 cursor-pointer hover:bg-gray-500">
+            <div
+              className="w-full flex justify-between items-center gap-x-4"
+              onClick={onClick}
+            >
               {title}
               {createSvgImage(title)}
             </div>
