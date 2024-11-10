cbb6cf56e59a4aab5ad16ea18024f2c8572ba840
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-11-10 18:19:48 -0300

    feat: add image in editable

--- src/app/user-form/page.jsx
+++ src/app/user-form/page.jsx
@@ -41,9 +41,13 @@ const formSchema = z.object({
 export default function PersonalDataForm() {
   const [isEditable, setIsEditable] = useState(false);
   const [imageFile, setImage] = useState();
+  const [oldImage, setOldImage] = useState('')
   const { data: response} = useSWR("/personal-data", fetcher, {
     onSuccess() {
       setIsEditable(true);
+      if(response?.image) {
+        setOldImage(response.image)
+      }
     }
   });
   const { trigger } = useSWRMutation("/personal-data", sendRequest);
@@ -107,7 +111,7 @@ export default function PersonalDataForm() {
             <Avatar className="w-[200px] h-[200px] m-5">
 
             <AvatarImage
-            src={ imageFile ? URL.createObjectURL(imageFile) :  "https://github.com/shadcn.png"}
+            src={ imageFile ? URL.createObjectURL(imageFile) :  (oldImage || "https://github.com/shadcn.png")}
             />
             </Avatar>
           <TooltipProvider >
